93115f479b0e98b30f602a6c578318adcb62ba94
NekoBard <tadeusz.m.lewandowski@gmail.com>
2016-06-23 23:15:33 +0200

    Add database settings, connection, connection error handling

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var config = require('./config.js');
+var mongoose = require('mongoose');
+
+mongoose.connect(config.db);
+
+mongoose.connection.on('connected', function () {
+  console.log('Connected');
+});
+
+mongoose.connection.on('error',function (err) {
+  console.log('connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+  console.log('disconnected');
+});
+
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
