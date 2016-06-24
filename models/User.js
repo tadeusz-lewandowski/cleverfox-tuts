b5f4f742e232b88b7ea629dcc7a036e00e9f5370
NekoBard <tadeusz.m.lewandowski@gmail.com>
2016-06-24 14:14:29 +0200

    Add bcrypt library and add user methods for generate hash and valid password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
@@ -7,5 +8,14 @@ var userSchema = new Schema({
   role:     { type: String, required: true, default: 'simple'},
   date:     { type: Date, default: Date.now }
 });
+
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = mongoose.model('User', tutorialSchema);
