cfd2f9687fea4d5a0c2716523d02e0bbae50f921
NekoBard <tadeusz.m.lewandowski@gmail.com>
2016-06-27 20:29:55 +0200

    Add authentication, test auth, create comments

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var config = require('./config.js');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
 var Tutorial = require('./models/Tutorial.js');
 var User = require('./models/User.js');
+require('./passport-config')(passport);
 
 mongoose.connect(config.db);
 
@@ -24,6 +28,66 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(session({ secret: 'topsecret',resave: true, saveUninitialized: true, cookie:{_expires : 60000 * 10} })); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
+
+function isLoggedIn(req, res, next) {
+    console.log(req.user);
+    if (req.isAuthenticated())
+        return next();
+
+    res.send('Authorize yourself');
+}
+
+app.post('/api/comments', function(req, res){
+  Tutorial.findById(req.body.id, function(err, tutorial) {
+    if(err){
+      res.sendStatus(404);
+    } else{
+      tutorial.comments.push({ content: req.body.content, username: req.body.username});
+
+      tutorial.save(function(err) {
+        if(err){
+          res.sendStatus(403);
+        } else{
+          res.sendStatus(200);
+        }
+      });
+    }
+  });
+});
+
+app.get('/api/user', isLoggedIn, function(req, res){
+  res.json({ username: req.user.username});
+});
+
+app.post('/api/signup',
+  passport.authenticate('local-signup'),
+  function(req, res) {
+    res.json({ id: req.user.id, username: req.user.username });
+  }
+);
+
+app.post('/api/login',
+  passport.authenticate('local-login'),
+  function(req, res) {
+    res.json({ id: req.user.id, username: req.user.username });
+  }
+);
+/*app.post('/api/login', function(req, res, next) {
+    passport.authenticate('local-login', function(err, user) {
+      if (err) { return next(err); }
+      if (!user) { return res.redirect('/login'); }
+      req.logIn(user, function(err) {
+        if (err) { return next(err); }
+        return res.send(user);
+      });
+    })(req, res, next);
+  });*/
+
 
 app.route('/api/tutorials')
   .get(function(req, res){
