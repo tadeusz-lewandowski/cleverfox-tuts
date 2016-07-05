5ec3bd2a059e2b5a856fc578884beffbb12a1910
NekoBard <tadeusz.m.lewandowski@gmail.com>
2016-07-05 14:46:47 +0200

    App component works properly

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,7 @@
-var Hello = React.createClass({
-  render: function() {
-    return (
-      <div>
-        Hello, world! I am a CommentBox.
-      </div>
-    );
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (<div>Hello my App</div>)
   }
-});
+}
---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+//import { Router, Route, Link, browserHistory } from 'react-router'
+import App from './components/App.jsx'
+
+
+render((
+  <App />
+), document.getElementById('app'));
